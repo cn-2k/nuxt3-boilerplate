74c540676a131bd35ce4aedbd5d0a37b3dc70b80
Caio Vinicius <caio.vinnicius2k@gmail.com>
2023-06-07 10:52:01 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <div
+      class="flex h-screen flex-col items-center justify-center text-5xl text-zinc-600"
+    >
+      Nuxt 3 + TailwindCSS + ESLint + Prettier Tailwind
+    </div>
+  </div>
+</template>
